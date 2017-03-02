c5b448de87a95371c798cc3ceb6991fb1427fe9d
handharbeni <mhandharbeni@gmail.com>
2017-03-02 15:21:38 +0700

    Disabling button click

--- application/modules/Pegawai/views/view.php
+++ application/modules/Pegawai/views/view.php
@@ -22,7 +22,7 @@
                  <td class="hidden-xs"><?=$spegawai['email'];?></td>
                  <td class="hidden-xs"><?=$spegawai['date_add'];?></td>
                  <td><div class="btn-group">
-                       <a class="btn btn-default" href="javascript:void(0)" data-toggle="popover" data-placement="left"  data-html="true" title='Hapus Data?' data-content='<a class="btn btn-danger" onclick="delRow(<?=$spegawai['id']?>)" href="#">Ya</a>'><i class="fa fa-times"></i></a>
+                       <a class="btn btn-default" href="javascript:void(0)" data-toggle="popover" data-placement="left"  data-html="true" title='Hapus Data?' data-content='<button class="btn btn-danger" onclick="delRow(<?=$spegawai['id']?>)" href="#" id="aConfirm">Ya</button>'><i class="fa fa-times"></i></a>
                        <a class="btn btn-default" data-toggle="tooltip" data-placement="top" title="Ubah Data" onclick="showDialogUpdate('<?=$spegawai['id']?>', this)"><i class="fa fa-pencil"></i></a>
                      </div>
                   </td>
@@ -182,13 +182,21 @@
           type: 'post',
           url: '<?=base_url('Pegawai/Master/delete')?>/'+id,
           data: $('form').serialize(),
+          beforeSend: function() { 
+            $("#aConfirm").html('<option> Loading ...</option>');
+            document.getElementById('aConfirm').setAttribute('disabled', 'disabled');
+          },
           success: function (i) {
             if(i==2){
               $('#tRow'+id).remove();
+              $("#aConfirm").html('Ya');
+              document.getElementById('aConfirm').removeAttribute('disabled');
             }else if(i==1){
-
+              $("#aConfirm").html('Ya');
+              document.getElementById('aConfirm').removeAttribute('disabled');
             }else if(1==0){
-
+              $("#aConfirm").html('Ya');
+              document.getElementById('aConfirm').removeAttribute('disabled');
             }
           }    
         });
