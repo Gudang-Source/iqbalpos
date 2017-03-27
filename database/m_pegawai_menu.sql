91c71b30494742522082d8505f4e2684d5fea7a1
Dimas Virdana <as5lang.s@gmail.com>
2017-03-27 08:24:53 +0700

    New field in tables

--- database/m_pegawai_menu.sql
+++ database/m_pegawai_menu.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 26, 2017 at 10:40 PM
+-- Generation Time: Mar 27, 2017 at 08:23 AM
 -- Server version: 5.6.21
 -- PHP Version: 5.6.3
 
@@ -29,22 +29,23 @@ SET time_zone = "+00:00";
 CREATE TABLE IF NOT EXISTS `m_pegawai_menu` (
 `id` int(11) NOT NULL,
   `nama` varchar(20) NOT NULL,
-  `icon_class` varchar(50) NOT NULL COMMENT 'additional class untuk icon menu'
+  `icon_class` varchar(50) NOT NULL COMMENT 'additional class untuk icon menu',
+  `deleted` tinyint(1) NOT NULL DEFAULT '1' COMMENT '1 = Aktif, 0 = Terhapus'
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `m_pegawai_menu`
 --
 
-INSERT INTO `m_pegawai_menu` (`id`, `nama`, `icon_class`) VALUES
-(1, 'master', ''),
-(2, 'produk', ''),
-(3, 'bahan baku', ''),
-(4, 'stok', ''),
-(5, 'transaksi', ''),
-(6, 'laporan', ''),
-(7, 'finance', ''),
-(8, 'log', '');
+INSERT INTO `m_pegawai_menu` (`id`, `nama`, `icon_class`, `deleted`) VALUES
+(1, 'master', '', 1),
+(2, 'produk', '', 1),
+(3, 'bahan baku', '', 1),
+(4, 'stok', '', 1),
+(5, 'transaksi', '', 1),
+(6, 'laporan', '', 1),
+(7, 'finance', '', 1),
+(8, 'log', '', 1);
 
 --
 -- Indexes for dumped tables
--- database/m_pegawai_permission.sql
+++ database/m_pegawai_permission.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 27, 2017 at 08:15 AM
+-- Generation Time: Mar 27, 2017 at 08:23 AM
 -- Server version: 5.6.21
 -- PHP Version: 5.6.3
 
@@ -31,51 +31,52 @@ CREATE TABLE IF NOT EXISTS `m_pegawai_permission` (
   `id_menu` int(11) NOT NULL,
   `nama` varchar(100) NOT NULL,
   `url` text NOT NULL COMMENT 'url segment setelah base_url()',
-  `icon_class` varchar(50) NOT NULL COMMENT 'additional class untuk icon menu'
+  `icon_class` varchar(50) NOT NULL COMMENT 'additional class untuk icon menu',
+  `deleted` tinyint(1) NOT NULL DEFAULT '1' COMMENT '1 = Aktif, 0 = Terhapus'
 ) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `m_pegawai_permission`
 --
 
-INSERT INTO `m_pegawai_permission` (`id`, `id_menu`, `nama`, `url`, `icon_class`) VALUES
-(1, 1, 'Hak Akses', 'index/modul/Master_hak_akses-master-index', ''),
-(2, 1, 'Pegawai', 'index/modul/Master_pegawai-master-index', ''),
-(3, 1, 'Customer', 'index/modul/Master_customer-master-index', ''),
-(4, 1, 'Customer Level', 'index/modul/Master_customer_level-master-index', ''),
-(5, 1, 'Supplier Produk', '', ''),
-(6, 1, 'Supplier Bahan', '', ''),
-(7, 1, 'Satuan', '', ''),
-(8, 1, 'Metode Pembayaran', '', ''),
-(9, 1, 'Provinsi', '', ''),
-(10, 1, 'Kota', '', ''),
-(11, 1, 'Lokasi Gudang', '', ''),
-(12, 2, 'Kategori', '', ''),
-(13, 2, 'Ukuran', '', ''),
-(14, 2, 'Warna', '', ''),
-(15, 2, 'Bahan', '', ''),
-(16, 2, 'Semua Produk', '', ''),
-(17, 2, 'Edisi Katalog', '', ''),
-(18, 3, 'Kategori', '', ''),
-(19, 3, 'Warna', '', ''),
-(20, 3, 'Semua Bahan', '', ''),
-(21, 4, 'Semua Stok', '', ''),
-(22, 4, 'Pesanan', '', ''),
-(23, 4, 'Service', '', ''),
-(24, 5, 'Purchase Order', '', ''),
-(25, 5, 'Pembelian', '', ''),
-(26, 5, 'Input Barang Masuk', '', ''),
-(27, 5, 'Penjualan', '', ''),
-(28, 5, 'Retur', '', ''),
-(29, 5, 'Dropship', '', ''),
-(30, 6, 'Penjualan', '', ''),
-(31, 6, 'Pembelian', '', ''),
-(32, 6, 'TOP Customer', '', ''),
-(33, 6, 'Best Seller', '', ''),
-(34, 6, 'Stok', '', ''),
-(35, 7, 'Kas Kecil', '', ''),
-(36, 7, 'Transfer Harian', '', ''),
-(37, 8, 'Log Aktivitas', '', '');
+INSERT INTO `m_pegawai_permission` (`id`, `id_menu`, `nama`, `url`, `icon_class`, `deleted`) VALUES
+(1, 1, 'Hak Akses', 'index/modul/Master_hak_akses-master-index', '', 1),
+(2, 1, 'Pegawai', 'index/modul/Master_pegawai-master-index', '', 1),
+(3, 1, 'Customer', 'index/modul/Master_customer-master-index', '', 1),
+(4, 1, 'Customer Level', 'index/modul/Master_customer_level-master-index', '', 1),
+(5, 1, 'Supplier Produk', '', '', 1),
+(6, 1, 'Supplier Bahan', '', '', 1),
+(7, 1, 'Satuan', '', '', 1),
+(8, 1, 'Metode Pembayaran', '', '', 1),
+(9, 1, 'Provinsi', '', '', 1),
+(10, 1, 'Kota', '', '', 1),
+(11, 1, 'Lokasi Gudang', '', '', 1),
+(12, 2, 'Kategori', '', '', 1),
+(13, 2, 'Ukuran', '', '', 1),
+(14, 2, 'Warna', '', '', 1),
+(15, 2, 'Bahan', '', '', 1),
+(16, 2, 'Semua Produk', '', '', 1),
+(17, 2, 'Edisi Katalog', '', '', 1),
+(18, 3, 'Kategori', '', '', 1),
+(19, 3, 'Warna', '', '', 1),
+(20, 3, 'Semua Bahan', '', '', 1),
+(21, 4, 'Semua Stok', '', '', 1),
+(22, 4, 'Pesanan', '', '', 1),
+(23, 4, 'Service', '', '', 1),
+(24, 5, 'Purchase Order', '', '', 1),
+(25, 5, 'Pembelian', '', '', 1),
+(26, 5, 'Input Barang Masuk', '', '', 1),
+(27, 5, 'Penjualan', '', '', 1),
+(28, 5, 'Retur', '', '', 1),
+(29, 5, 'Dropship', '', '', 1),
+(30, 6, 'Penjualan', '', '', 1),
+(31, 6, 'Pembelian', '', '', 1),
+(32, 6, 'TOP Customer', '', '', 1),
+(33, 6, 'Best Seller', '', '', 1),
+(34, 6, 'Stok', '', '', 1),
+(35, 7, 'Kas Kecil', '', '', 1),
+(36, 7, 'Transfer Harian', '', '', 1),
+(37, 8, 'Log Aktivitas', '', '', 1);
 
 --
 -- Indexes for dumped tables
